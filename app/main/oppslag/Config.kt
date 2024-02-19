8c7f3d929c5660544162d03d736182204a2475de
Sturle Helland <sturle.helland@nav.no>
2024-02-19 11:18:39 +0100

    Lagt til riktig uri lenke

--- app/main/oppslag/Config.kt
+++ app/main/oppslag/Config.kt
@@ -18,7 +18,7 @@ data class Config(
         clientId = getEnvVar("AZURE_APP_CLIENT_ID"),
         clientSecret = getEnvVar("AZURE_APP_CLIENT_SECRET"),
         tokenEndpoint = URI.create(getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")).toURL(),
-        jwks =  URI.create(getEnvVar("AZURE_APP_JWKS")).toURL(),
+        jwks =  URI.create(getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI")).toURL(),
         issuer = getEnvVar("AZURE_OPENID_CONFIG_ISSUER")
     ),
     val pdlConfig: PdlConfig = PdlConfig(),

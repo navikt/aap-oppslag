ba85aee12aad01c8b357ce1768482a33a2e9a563
Nicolas N. <nicolas.nordhagen@nav.no>
2026-07-31 09:10:44 +0200

    AAP-2194 Forenkle og rydde opp CallLogging

--- app/main/oppslag/App.kt
+++ app/main/oppslag/App.kt
@@ -10,10 +10,10 @@ import io.ktor.server.auth.authenticate
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.callid.callIdMdc
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.statuspages.StatusPages
-import io.ktor.server.request.header
 import io.ktor.server.request.httpMethod
 import io.ktor.server.request.path
 import io.ktor.server.response.respondText
@@ -22,6 +22,7 @@ import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import no.nav.aap.komponenter.server.auth.IdentityProvider
 import no.nav.aap.komponenter.server.authentication
+import no.nav.aap.komponenter.server.common.MdcKeys
 import oppslag.integrasjoner.behandler.BehandlerClient
 import oppslag.integrasjoner.krr.KrrClient
 import oppslag.integrasjoner.pdl.PdlException
@@ -36,7 +37,6 @@ import oppslag.routes.pdlRoute
 import oppslag.routes.safRoute
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.event.Level
 
 val LOGGER: Logger = LoggerFactory.getLogger("aap-oppslag")
 
@@ -59,17 +59,9 @@ fun Application.api(
     authentication(listOf(IdentityProvider.TOKENX, IdentityProvider.ENTRA_ID))
 
     install(CallLogging) {
-        level = Level.INFO
-        logger = LOGGER
-        format { call ->
-            """
-                URL:            ${call.request.local.uri}
-                Status:         ${call.response.status()}
-                Method:         ${call.request.httpMethod.value}
-                User-agent:     ${call.request.headers["User-Agent"]}
-                CallId:         ${call.request.header("x-callId") ?: call.request.header("nav-callId")}
-            """.trimIndent()
-        }
+        callIdMdc(MdcKeys.CallId)
+        mdc(MdcKeys.Method) { call -> call.request.httpMethod.value }
+        mdc(MdcKeys.InboundUri) { call -> call.request.path() }
         filter { call -> call.request.path().startsWith("/actuator").not() }
     }
 
